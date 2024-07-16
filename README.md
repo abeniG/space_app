d4bf30ae481fec6003a0fc2e2ef59b4bfccf7145
Abenezer <abenzerdeveloper@gmail.com>
2024-07-16 01:04:56 -0700

    Revert "all files"
    This reverts commit a9f89ef5ad116b04a7ab2ff061ca36b07186430c.

--- README.md
+++
@@ -1,3 +0,0 @@
-# space_app
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/example/space_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.space_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
